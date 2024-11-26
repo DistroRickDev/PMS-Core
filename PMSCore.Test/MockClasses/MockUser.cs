e8bd0084fef8f44ef06b48619b15ed8becd9e06a
JDax <jessica.dax.email@gmail.com>
2024-11-26 16:47:39 +0000

    Added mock class folder to test section. along with bare bones skeleton for a Mock User class to test the interface with.

---
+++ PMSCore.Test/MockClasses/MockUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMSCore.Test.MockClasses
+{
+    internal class MockUser
+    {
+    }
+}

b4defa549a5c5f88ea7804fa09ffd2d587493991
JDax <jessica.dax.email@gmail.com>
2024-11-27 17:53:24 +0000

    Added Mock user class skeleton for tests. Not implemented yet.

--- PMSCore.Test/MockClasses/MockUser.cs
+++ PMSCore.Test/MockClasses/MockUser.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace PMSCore.Test.MockClasses
 {
-    internal class MockUser
+    public class MockUser : IUserInterface
     {
+        private string UserID;
+        private Permission Permission;
+
+
+        public Permission GetPermission()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUserID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Register(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPermission(Permission permission)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

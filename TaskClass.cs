e018cbd6b34219278cfdc72b3df9619f4aca8cf8
LordB0red <134875649+LordB0red@users.noreply.github.com>
2024-11-28 22:58:22 +0000

    Updated Task class, based on feedback

--- TaskClass.cs
+++ TaskClass.cs
@@ -2,10 +2,10 @@
 
 public enum TaskStatus
 {
-	NotStarted,
+	New,
 	InProgress,
 	Completed,
-	OnHold       // If some issue prevents progression on a task
+	Blocked       // If some issue prevents progression on a task
 }
 
 // Enum representing priotity level of a task
@@ -22,14 +22,10 @@ public class Task
 {
 	// Properties for a task object
 
-	public string TaskName { get; private set; }
-	public string Description { get; private set; }
-	public TaskStatus Status { get; private set; }
-	public TaskPriority Priority { get; private set; }
-	public DateTime CreatedOn { get; private set; } // Timestamping the creation of a task
-	public DateTime? CompletedOn { get; private set; } // also the completion date
-	public DateTime? DueDate { get; private set; }  // Optional parameter for a deadline
-	public string IsAssignedTo { get; private set; }
+	public string TaskName { get; set; }
+	public string Description { get; set; }
+	public TaskStatus Status { get; set; }
+	public TaskPriority Priority { get; set; }
 
 
 	// Constructor for intializing new tasks
@@ -39,57 +35,30 @@ public class Task
 		if (string.IsNullOrEmpty(taskName)) // Validation for task name
 			throw new ArgumentNullException("Task must have a name");
 
+		if (string.IsNullOrEmpty(description))
+			throw new ArgumentNullException("Task must have a description");
+
 		// Properties initialized
 
 		TaskName = taskName;
 		Description = description;
 		Priority = priority;
-		Status = TaskStatus.NotStarted; // A new task hasn't been started as default
-		CreatedOn = DateTime.Now; // To record creation time
-		DueDate = dueDate;
+		Status = TaskStatus.New; // A new task hasn't been started as default
 	}
 
 	// Method for changing task status
 
 	public void ChangeTaskStatus(TaskStatus newStatus)
 	{
-		if (Status == TaskStatus.Completed) // Finished task's shouldn't be modified
-			throw new InvalidOperationException("Status of a completed task should not be changed");
-
 		Status = newStatus;
-
-		if (newStatus == TaskStatus.Completed)
-		{
-			CompletedOn = DateTime.Now; // Timestamp set when task is completed
-		}
-
 	}
 
-	// Method to assign a task to a user
-
-	public void AssignToUser(string username)
-	{
-		if (string.IsNullOrWhiteSpace(username))
-			throw new ArgumentException("The username should not be empty");
-
-		IsAssignedTo = username;
-	}
 
 	// Boolean method for checking if further action is required for a task
 
 	public bool ActionRequired()
 	{
 		return Status != TaskStatus.Completed; // Return will be true (action is required) if status isn't "Completed"
-	}
-
-	// Method to check if a task is overdue
-
-	public bool IsTaskOverdue()
-	{
-		if (!DueDate.HasValue) return false; // If there's not due date, a task cannot be overdue
-
-		// Checking if current date is past due date & task is not "Completed"
-		return DateTime.Now > DueDate.Value && Status != TaskStatus.Completed;
 	}
 
 	// Method used to display all details of a task
@@ -100,28 +69,6 @@ public class Task
 		Console.WriteLine($"Description: {Description}");
         Console.WriteLine($"Priority: {Priority}");
         Console.WriteLine($"Status: {Status}");
-        Console.WriteLine($"Created On: {CreatedOn}");
-
-		// To display the task's assigned user or default to "Not assigned"
-		Console.WriteLine($"Task Assigned To: {IsAssignedTo ?? "Not Assigned"}");
-
-        // To display a due date if one is set
-        if (DueDate.HasValue)
-        {
-            Console.WriteLine($"Task Due Date: {DueDate.Value}");
-        }
-
-		// Display completion time if task is finished
-		if (CompletedOn.HasValue)
-        {
-            Console.WriteLine($"Task Completed On: {CompletedOn.Value}");
-        }
-
-        // To show when a task is overdue
-        if (IsTaskOverdue())
-        {
-            Console.WriteLine("The task is currently overdue");
-        }
 
     }
 

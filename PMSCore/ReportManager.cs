d765fb433010e63cb62eb87c96b05f182856fb3e
ivanmartin91 <151534113+ivanmartin91@users.noreply.github.com>
2024-12-10 19:30:31 +0000

    Adding ReportManager class which will handle report generation for either tasks or projects

---
+++ PMSCore/ReportManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using PMSCore;
+
+namespace PMSCore
+{
+    /// <summary>
+    /// This class is used to handle and create reports.
+    /// </summary>
+    public class ReportManager
+    {
+        /// <summary>
+        /// Makes a report from a project.
+        /// </summary>
+        /// <param name="project">The project to get the report from.</param>
+        /// <returns>A string that has the project report.</returns>
+        public string GenerateProjectReport(Project project)
+        {
+            if (project == null)
+            {
+                throw new ArgumentNullException(nameof(project), "Project cannot be null.");
+            }
+
+            return project.GenerateReport();
+        }
+
+        /// <summary>
+        /// Makes a report from a task.
+        /// </summary>
+        /// <param name="task">The task to get the report from.</param>
+        /// <returns>A string that has the task report.</returns>
+        public string GenerateTaskReport(Task task)
+        {
+            if (task == null)
+            {
+                throw new ArgumentNullException(nameof(task), "Task cannot be null.");
+            }
+
+            return task.GenerateReport();
+        }
+    }
+}
